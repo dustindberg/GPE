c375cec9bb4c2fc024e068016694b19b0d72533b
skref <dlindberg@tulane.edu>
2020-05-28 16:47:20 -0500

    Runs with sinusoidal asymmetric potential

--- asymmetric_tunneling.py
+++ asymmetric_tunneling.py
@@ -9,10 +9,13 @@ g = 2194.449140
 propagation_dt = 1e-4
 
 #height of asymmetric barrier
-height_asymmetric = 1e2
+height_asymmetric = 7.5e2
 
 #This corresponds to sharpness parameter
-delta = 3.5
+delta = 10
+
+#Increases the number of peaks for Option 2
+osc = (2/3)
 
 @njit
 def v(x, t=0.):
@@ -20,9 +23,9 @@ def v(x, t=0.):
     Potential energy
     """
     #Option 1
-    return 0.5 * x ** 2 + x ** 2 * height_asymmetric * np.exp(-(x / delta) ** 2) * (x < 0)
+    #return 0.5 * x ** 2 + x ** 2 * height_asymmetric * np.exp(-(x / delta) ** 2) * (x < 0)
     #Option 2
-    #return 0.5 * x ** 2 + height_asymmetric * np.sin(x)**2 * np.exp(-(x / delta) **2 ) * (x<0)
+    return 0.5 * x ** 2 + height_asymmetric * np.sin(osc * x) ** 2 * np.exp(-(x / delta) ** 2) * (x < 0)
 
 @njit
 def diff_v(x, t=0.):
@@ -30,10 +33,10 @@ def diff_v(x, t=0.):
     the derivative of the potential energy for Ehrenfest theorem evaluation
     """
     #Option 1
-    return x + (2. * x - 2. * (1. / delta) ** 2 * x ** 3) * height_asymmetric * np.exp(-(x / delta) ** 2) * (x < 0)
+    #return x + (2. * x - 2. * (1. / delta) ** 2 * x ** 3) * height_asymmetric * np.exp(-(x / delta) ** 2) * (x < 0)
     #Option 2
-    #return x + (2*np.sin(x)*np.cos(x) - 2. * x * (1. / delta) ** 2 * np.sin(x) ** 2) * height_asymmetric np.exp(-(x / delta) **2 ) * (x<0)
-    #run tomorrow
+    return x + (2 * osc * np.sin(osc * x) * np.cos(osc * x) - 2. * x * (1. / delta) ** 2 * np.sin(osc * x) ** 2) * height_asymmetric * np.exp(-(x / delta) ** 2) * (x < 0)
+
 
 @njit
 def diff_k(p, t=0.):
